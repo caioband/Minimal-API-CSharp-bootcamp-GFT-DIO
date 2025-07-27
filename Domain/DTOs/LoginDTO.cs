5d030c7e0e1d944338b89929f0b5d75b2ab42f4c
Caio <caioobsantos@gmail.com>
2025-07-27 02:27:12 -0300

    hehe boi

---
+++ Domain/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+namespace Minimal.Domain.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ Infra/DBContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Minimal.Infra.DB
+{
+    public class InfraDbContext : DbContext
+    {
+
+    }
+    
+}
